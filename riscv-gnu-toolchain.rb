55d66be0360e7f05bb868ae2f9db450cd04da4cb
yoshiok <yoshiok@localhost>
2020-04-15 15:42:23 +0900

    update

--- riscv-gnu-toolchain.rb
+++ riscv-gnu-toolchain.rb
@@ -3,8 +3,8 @@ require "formula"
 class RiscvGnuToolchain < Formula
   homepage "http://riscv.org"
   # url "https://github.com/riscv/riscv-gnu-toolchain.git"
-  # version "master"
   url "https://github.com/riscv/riscv-gnu-toolchain.git", :branch => "rvv-0.7.x"
+  version "master"
 
   # bottle do
   # root_url 'http://riscv.org.s3.amazonaws.com/bottles'
