ac3051da6f0cb9e78301d3289f6d860f1ad6f1f2
yoshiok <yoshiok@localhost>
2020-04-15 18:49:34 +0900

    update

--- riscv-pk.rb
+++ riscv-pk.rb
@@ -5,11 +5,11 @@ class RiscvPk < Formula
   url "https://github.com/riscv/riscv-pk.git"
   version "master"
 
-  bottle do
-    root_url 'http://riscv.org.s3.amazonaws.com/bottles'
-    cellar :any_skip_relocation
-    sha256 "2c7e91b169bca977800ca1d14cd7f2fc7494b5729f3e18016fec73fbf832fed2" => :catalina
-  end
+  # bottle do
+  #   root_url 'http://riscv.org.s3.amazonaws.com/bottles'
+  #  cellar :any_skip_relocation
+  #  sha256 "2c7e91b169bca977800ca1d14cd7f2fc7494b5729f3e18016fec73fbf832fed2" => :catalina
+  # end
 
   depends_on "riscv-isa-sim" => :build
   depends_on "riscv-gcc" => :build
